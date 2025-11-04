0cdfad3856eede842cdea2815b92c77511c6f777
synergiatechexperts <tomas@synergiatechexperts.com>
2025-11-04 17:06:03 +0100

    Optimize ProyectoCatalog admin changelist

--- model/admin.py
+++ model/admin.py
@@ -2,6 +2,16 @@
 from django.contrib import admin
 from .models import ProyectoCatalog, Cliente, OpenAIConfig
 
-admin.site.register(ProyectoCatalog)
+
+@admin.register(ProyectoCatalog)
+class ProyectoCatalogAdmin(admin.ModelAdmin):
+    list_display = ("nombre_catalogo", "version", "fecha_actualizacion")
+    ordering = ("-version",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.only("id", "nombre_catalogo", "version", "fecha_actualizacion")
+
+
 admin.site.register(Cliente)
 admin.site.register(OpenAIConfig)
